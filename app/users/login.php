037dc8a0c45433295a902df45fdbcc1f02fdb2de
milliebase <betsyalva@milliebase.se>
2019-12-20 15:52:31 +0100

    Update with fetchEmail-function

--- app/users/login.php
+++ app/users/login.php
@@ -7,22 +7,7 @@ require __DIR__ . '/../autoload.php';
 if (isset($_POST['email'], $_POST['password'])) {
     $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
 
-    //Prepare the statement
-    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-
-    if (!$statement) {
-        redirect('/../login.php');
-    }
-
-    //Bind the user input of email with email in statement
-    $statement->bindParam(':email', $email, PDO::PARAM_STR);
-
-    //Execute your query
-    $statement->execute();
-
-    //Get the first item in database with fetch, if I wanted to get more users
-    //I would have used fetchAll and I would fetch an array.
-    $user = $statement->fetch(PDO::FETCH_ASSOC);
+    $user = fetchEmail($pdo, $email);
 
     if (password_verify($_POST['password'], $user['password'])) {
         //Protect password from showing in browser
