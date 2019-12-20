69c2f8e9a46423fdf16f0f09875a8ad4a2bd503f
milliebase <betsyalva@milliebase.se>
2019-12-20 15:54:44 +0100

    Added functions for sign up

--- app/functions.php
+++ app/functions.php
@@ -16,3 +16,67 @@ if (!function_exists('redirect')) {
         exit;
     }
 }
+
+if (!function_exists('fetchEmail')) {
+    /**
+     * Fetch emailadress from database.
+     *
+     * @param PDO $pdo
+     * @param string $email
+     *
+     * @return array
+     */
+    function fetchEmail(PDO $pdo, string $email)
+    {
+        $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+
+        $statement->execute([
+            ':email' => $email,
+        ]);
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+}
+
+if (!function_exists('unsetErrors')) {
+    /**
+     * On each page load except signup the error messages will be cleared.
+     *
+     * @return void
+     */
+    function unsetErrors()
+    {
+        if ($_SERVER['REQUEST_URI'] != '/signup.php') {
+            unset($_SESSION['errors']);
+        }
+    }
+}
+
+if (!function_exists('unsetErrorType')) {
+    function unsetErrorType(string $errorType)
+    {
+        unset($_SESSION['errors'][$errorType]);
+    }
+}
+
+if (!function_exists('handleErrors')) {
+    function handleErrors(string $errorType, string $errorMessage)
+    {
+        foreach ($_SESSION['errors'] as $sessionError) {
+            if ($sessionError === $errorMessage) {
+                redirect('/signup.php');
+            }
+        }
+
+        $_SESSION['errors'][$errorType] = $errorMessage;
+    }
+}
+
+if (!function_exists('isEmpty')) {
+    function isEmpty($string)
+    {
+        if ($string === '') {
+            return true;
+        }
+    }
+}
