9aadeea41c4effb48c6a9733320532a834728c6d
milliebase <betsyalva@milliebase.se>
2020-01-15 10:58:01 +0100

    updated delete-post function

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -10,7 +10,6 @@ if (isset($_POST['delete'], $_GET['post_id'])) {
     $postId = (int) trim(filter_var($_GET['post_id'], FILTER_SANITIZE_NUMBER_INT));
 
     $statement = $pdo->prepare('SELECT image FROM posts WHERE id = ? AND user_id = ?');
-
     $statement->execute([$postId, $userId]);
 
     $image = $statement->fetch(PDO::FETCH_ASSOC);
@@ -18,8 +17,10 @@ if (isset($_POST['delete'], $_GET['post_id'])) {
     unlink(__DIR__ . '/../../uploads/' . $image['image']);
 
     $statement = $pdo->prepare('DELETE FROM posts WHERE id = ? AND user_id = ?');
-
     $statement->execute([$postId, $userId]);
+
+    $statement = $pdo->prepare('DELETE FROM post_likes WHERE post_id = ?');
+    $statement->execute([$postId]);
 }
 
 redirect('/profile.php?username=' . $user['username']);
